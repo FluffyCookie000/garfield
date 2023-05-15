677d07a96d633b98af6ebcbedcbea8b0268664ea
FluffyCookie000 <fluffycookiecat000@gmail.com>
2023-05-15 11:49:59 -0400

    jlkm.kj

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ repo = Repo("C:/Users/fluff/Downloads/CODE/OTHER/garfield/garfield/.git")
 
 while n < 2:
     f = open(f"test/myfile{n}.txt", "x")
-    n = n + 1
     repo.index.add([f'test/myfile{n}.txt'])
+    n = n + 1
 
 
 
