b1821ba70a778457ca5fe432ad010bd63c13722e
FluffyCookie000 <fluffycookiecat000@gmail.com>
2023-05-15 11:47:09 -0400

    fad

--- test.py
+++ test.py
@@ -3,6 +3,16 @@ from git import Repo
 
 n=0
 
+repo = Repo("C:/Users/fluff/Downloads/CODE/OTHER/garfield/garfield/.git")
+
+
 while n == 2:
     f = open(f"test/myfile{n}.txt", "x")
     n = n + 1
+    repo.index.add([f'test/myfile{n}.txt'])
+
+
+
+    repo.index.commit(f'testing')
+    origin = repo.remote('origin')
+    origin.push()
